15b7b88e6751e7ff30e7026db9231a155e660386
X <xavier@learn2earnfresno.org>
2018-04-11 20:55:14 -0700

    Committed my Notes

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,45 @@
 // myName = new String(myName);
 // console.log(myName.toUpperCase());
 
-var myName = 43;
-console.log(typeof (myName));
-console.log(myName);
+// var myName = 43;
+// console.log(typeof (myName));
+// console.log(myName);
+
+
+// console.log(myName.toUpperCase());
+// var myName = "Martinez";
+//
+// var  test;
+//
+// console.log(test);
+
+// var myName = "Martinez";
+//
+// console.log(myName, "one");
+// {
+//     let myName = "Xavier";
+//     console.log(myName, "Two");
+// }
+//
+// console.log(myName, "Three");
+
+// const myName = "Martinez";
+//
+// console.log(myName, "one");
+// {
+//     let myName = "Xavier";
+//     console.log(myName, "Two");
+// }
+//
+// console.log(myName, "Three");
+
+// How we do this
+
+let birthday = 'jul 10 74';
+let rightnow = new Date(birthday);
+
+alert(rightnow);
+
+//Math OBJECT
+
+let String()
