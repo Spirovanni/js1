aa3fa1930b1d7048671028e0173fd74abddee29e
X <xavier@learn2earnfresno.org>
2018-04-11 19:21:30 -0700

    Another 2 day update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 // var myName = "Xavier";
 // var myLastName = "Martinez";
 
-var myHistory = "I want brush up my JavaScript in order to get better at Angular5 for a project.  In order to compete in 59Days of Code.";
-window.alert(myHistory);
+// var myHistory = "I want brush up my JavaScript in order to get better at Angular5 for a project.  In order to compete in 59Days of Code.";
+// window.alert(myHistory);
+
+// var myName = "Xavier";
+// myName = new String(myName);
+// console.log(myName.toUpperCase());
+
+var myName = 43;
+console.log(typeof (myName));
+console.log(myName);
