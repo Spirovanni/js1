41ffa464ca98e5f0f4a0388bf109c0f9c436126c
X <xavier@learn2earnfresno.org>
2018-04-11 18:18:08 -0700

    Another 2 day update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // var myName = "Xavier";
 // var myLastName = "Martinez";
 
-var myHistory = "I want brush up my JavaScript in order to get better at Angular5 for a project.  In order to compete in 59Days of Code."
+var myHistory = "I want brush up my JavaScript in order to get better at Angular5 for a project.  In order to compete in 59Days of Code.";
 window.alert(myHistory);
